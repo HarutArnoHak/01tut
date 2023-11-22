30f90aa83788a7aaf7d42f0b8c516d39ccfd9b34
haruthak <hak0887@gmail.com>
2023-11-22 14:27:47 -0800

    add use effect

--- src/ItemList.js
+++ src/ItemList.js
@@ -1,5 +1,4 @@
-
-import {FaTrashAlt} from 'react-icons/fa'
+import LineItem from './LineItem'
 
 const ItemList = ({items, handleCheck, handleDelete}) => {
   return (
@@ -7,12 +6,8 @@ const ItemList = ({items, handleCheck, handleDelete}) => {
             
             {
                 items.map((item)=>(
-                    <li className="item" key={item.id}>
-                        <input type="checkbox" checked={item.checked} onChange={()=>{handleCheck(item.id)}}/>
-                        <label onDoubleClick={()=>{handleCheck(item.id)}}>{item.item}</label>
-                        <FaTrashAlt role="button" tabIndex='0' onClick={()=>handleDelete(item.id)}/>
-                    </li>
 
+                    <LineItem key={item.id} item={item} handleCheck={handleCheck} handleDelete={handleDelete}/>
                 ))
             }
         </ul>
